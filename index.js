b13f4c31ed42d9a856c7241fa484ebdddc5ebf83
Liza McLain <lizamclain@gmail.com>
2022-12-13 19:25:37 -0600

    commit tree 51c1b906ff55e9377fc05b1f15848982e3d01234

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+const messages = []
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+writeCards(names)
+
+function countDown() {
+    let myNumber = 10;
+    while (myNumber >= 0) {
+        console.log(myNumber--);
+    }
+}
+countDown(myNumber);
